c2bd90d473e332a1b4eff10b019d41625051b560
Andrew Craigie <andrew@craigie.co>
2018-08-11 09:38:35 +0100

    Update gym.sql - working

--- app/db/gym.sql
+++ app/db/gym.sql
@@ -3,81 +3,28 @@ DROP TABLE IF EXISTS messages;
 DROP TABLE IF EXISTS payments;
 
 DROP TABLE IF EXISTS persons_memberships;
-DROP TABLE IF EXISTS persons_sessions;
-DROP TABLE IF EXISTS sessions;
-DROP TABLE IF EXISTS persons;
-
 DROP TABLE IF EXISTS memberships;
-
-DROP TABLE IF EXISTS playlist_songs;
-DROP TABLE IF EXISTS gym_classes_categories;
-DROP TABLE IF EXISTS gym_classes;
-
-DROP TABLE IF EXISTS roles;
 DROP TABLE IF EXISTS membership_types;
 
+DROP TABLE IF EXISTS persons_sessions;
+DROP TABLE IF EXISTS sessions;
 DROP TABLE IF EXISTS rooms;
+DROP TABLE IF EXISTS persons;
+DROP TABLE IF EXISTS roles;
+
+DROP TABLE IF EXISTS gym_classes_categories;
 DROP TABLE IF EXISTS gym_class_categories;
+
+DROP TABLE IF EXISTS playlist_songs;
+DROP TABLE IF EXISTS gym_classes;
 DROP TABLE IF EXISTS intensities;
+
 DROP TABLE IF EXISTS songs;
 
-CREATE TABLE songs(
-  id SERIAL8 PRIMARY KEY,
-  name VARCHAR(255),
-  artist VARCHAR(255),
-  duration INTERVAL
-);
-
-CREATE TABLE intensities(
-  id SERIAL8 PRIMARY KEY,
-  name VARCHAR(255)
-);
-
-CREATE TABLE gym_class_categories(
-  id SERIAL8 PRIMARY KEY,
-  name VARCHAR(255),
-  description VARCHAR(255)
-);
-
-CREATE TABLE rooms(
-  id SERIAL8 PRIMARY KEY,
-  name VARCHAR(255)
-);
-
-CREATE TABLE membership_types(
- id SERIAL8 PRIMARY KEY,
- name VARCHAR(255),
- description VARCHAR(255)
-);
 
 CREATE TABLE roles(
   id SERIAL8 PRIMARY KEY,
   name VARCHAR(255)
-);
-
-CREATE TABLE gym_classes(
-  id SERIAL8 PRIMARY KEY,
-  name VARCHAR(255)
-  description TEXT,
-  intensity_id INT REFERENCES intesities(id) ON DELETE CASCADE
-);
-
-CREATE TABLE gym_class_categories(
-  id SERIAL8 PRIMARY KEY,
-  class_id INT REFERENCES gym_classes(id) ON DELETE CASCADE
-);
-
-CREATE TABLE playlist_songs(
-  id SERIAL8 PRIMARY KEY,
-  class_id INT REFERENCES gym_classes(id) ON DELETE CASCADE
-);
-
-CREATE TABLE memberships(
-  id SERIAL8 PRIMARY KEY,
-  membership_number INT,
-  start_date TIMESTAMP,
-  end_date TIMESTAMP,
-  membership_type_id INT REFERENCES membership_types(id) ON DELETE CASCADE
 );
 
 CREATE TABLE persons(
@@ -93,6 +40,75 @@ CREATE TABLE persons(
   photo_url VARCHAR(255),
   role_id INT REFERENCES roles(id) ON DELETE CASCADE
 );
+
+CREATE TABLE membership_types(
+ id SERIAL8 PRIMARY KEY,
+ name VARCHAR(255),
+ description VARCHAR(255)
+);
+
+CREATE TABLE memberships(
+  id SERIAL8 PRIMARY KEY,
+  membership_number INT,
+  start_date TIMESTAMP,
+  end_date TIMESTAMP,
+  membership_type_id INT REFERENCES membership_types(id) ON DELETE CASCADE
+);
+
+CREATE TABLE persons_memberships(
+  id SERIAL8 PRIMARY KEY,
+  person_id INT REFERENCES persons(id) ON DELETE CASCADE,
+  membership_id INT REFERENCES memberships(id) ON DELETE CASCADE
+);
+
+CREATE TABLE payments(
+  id SERIAL8 PRIMARY KEY,
+  description VARCHAR(255),
+  amount DECIMAL,
+  person_id INT REFERENCES persons(id) ON DELETE CASCADE
+);
+
+
+CREATE TABLE messages(
+  id SERIAL8 PRIMARY KEY,
+  message TEXT,
+  date_time_added TIMESTAMP,
+  person_id INT REFERENCES persons(id) ON DELETE CASCADE
+);
+
+CREATE TABLE rooms(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+
+
+CREATE TABLE gym_class_categories(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  description VARCHAR(255)
+);
+
+CREATE TABLE intensities(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255)
+);
+
+CREATE TABLE gym_classes(
+  id SERIAL8 PRIMARY KEY,
+  name VARCHAR(255),
+  description TEXT,
+  intensity_id INT REFERENCES intensities(id) ON DELETE CASCADE
+);
+
+CREATE TABLE gym_classes_categories(
+  id SERIAL8 PRIMARY KEY,
+  class_id INT REFERENCES gym_classes(id) ON DELETE CASCADE,
+  class_category_id INT REFERENCES gym_class_categories(id) ON DELETE CASCADE
+);
+
+
+
 
 CREATE TABLE sessions(
   id SERIAL8 PRIMARY KEY,
@@ -113,23 +129,14 @@ CREATE TABLE persons_sessions(
   session_id INT REFERENCES sessions(id) ON DELETE CASCADE
 );
 
-CREATE TABLE persons_memberships(
+CREATE TABLE songs(
   id SERIAL8 PRIMARY KEY,
-  person_id INT REFERENCES pesons(id) ON DELETE CASCADE,
-  membership_id INT REFERENCES memberships(id) ON DELETE CASCADE
+  name VARCHAR(255),
+  artist VARCHAR(255),
+  duration INTERVAL
 );
 
-CREATE TABLE payments(
+CREATE TABLE playlist_songs(
   id SERIAL8 PRIMARY KEY,
-  description VARCHAR(255),
-  amount DECIMAL,
-  person_id INT REFERENCES pesons(id) ON DELETE CASCADE
-);
-
-
-CREATE TABLE messages(
-  id SERIAL8 PRIMARY KEY,
-  message TEXT,
-  date_time_added TIMESTAMP,
-  person_id INT REFERENCES persons(id) ON DELETE CASCADE
+  class_id INT REFERENCES gym_classes(id) ON DELETE CASCADE
 );
