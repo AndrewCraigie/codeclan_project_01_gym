3b6c2e7055a3b4ad8c6108dcf06e35fb55adfc8b
Andrew Craigie <andrew@craigie.co>
2018-08-14 06:52:33 +0100

    Amend gym_class_categories erbs

--- app/controllers/gym_class_categories_controller.rb
+++ app/controllers/gym_class_categories_controller.rb
@@ -8,13 +8,13 @@ also_reload( '../models/*' )
 
 
 get '/gym_class_categories/sort/:direction/:prop' do
-  @gym_class_categories = GymClassesCategory.all_ordered(params[:prop], params[:direction])
+  @gym_class_categories = GymClassCategory.all_ordered(params[:prop], params[:direction])
   erb ("gym_class_categories/index".to_sym)
 end
 
 # INDEX route
 get '/gym_class_categories' do
-  @gym_class_categories = GymClassesCategory.all()
+  @gym_class_categories = GymClassCategory.all()
   erb ("gym_class_categories/index".to_sym)
 end
 
@@ -24,34 +24,34 @@ end
 
 # DESTROY
 post '/gym_class_categories/:id/delete' do
-  @gym_class_category = GymClassesCategory.find_by_id(params[:id])
+  @gym_class_category = GymClassCategory.find_by_id(params[:id])
   @gym_class_category.delete()
   redirect ('/gym_class_categories')
 end
 
 # EDIT
 get '/gym_class_categories/:id/edit' do
-  @gym_class_category = GymClassesCategory.find_by_id(params['id'].to_i)
+  @gym_class_category = GymClassCategory.find_by_id(params['id'].to_i)
   erb ("gym_class_categories/edit".to_sym)
 end
 
 # SHOW route
 get '/gym_class_categories/:id' do
-  @gym_class_category = GymClassesCategory.find_by_id(params[:id])
+  @gym_class_category = GymClassCategory.find_by_id(params[:id])
   erb('gym_class_categories/show'.to_sym)
 end
 
 
 # UPDATE
 post '/gym_class_categories/:id' do
-  @gym_class_category = GymClassesCategory.new(params)
+  @gym_class_category = GymClassCategory.new(params)
   @gym_class_category.update()
   redirect ('/gym_class_categories')
 end
 
 # CREATE
 post '/gym_class_categories' do
-  @gym_class_category = GymClassesCategory.new(params)
+  @gym_class_category = GymClassCategory.new(params)
   @gym_class_category.save()
   redirect ('/gym_class_categories')
 end
