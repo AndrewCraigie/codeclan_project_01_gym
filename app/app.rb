63d577ff90d6b6345833751230139a7f4d9208c8
Andrew Craigie <andrew@craigie.co>
2018-08-14 15:37:48 +0100

    Add sessions controller

--- app/app.rb
+++ app/app.rb
@@ -11,6 +11,8 @@ require_relative('controllers/gym_class_categories_controller')
 require_relative('controllers/songs_controller')
 require_relative('controllers/persons_controller')
 require_relative('controllers/gym_classes_controller')
+require_relative('controllers/sessions_controller')
+
 also_reload( '../models/*' )
 
 
---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,55 @@
+require( 'sinatra' )
+require( 'sinatra/contrib/all' )
+require( 'pry')
+require_relative( '../models/session' )
+require_relative( '../models/person_session' )
+also_reload( '../models/*' )
+
+get '/sessions/sort/:direction/:prop' do
+  @sessions = Session.all_ordered(params[:prop], params[:direction])
+  erb ("sessions/index".to_sym)
+end
+
+# INDEX route
+get '/sessions' do
+  @sessions = Session.all()
+  erb ("sessions/index".to_sym)
+end
+
+get '/sessions/new' do
+  erb('sessions/new'.to_sym)
+end
+
+# DESTROY
+post '/sessions/:id/delete' do
+  @session = Session.find_by_id(params[:id])
+  @session.delete()
+  redirect ('/sessions')
+end
+
+# EDIT
+get '/sessions/:id/edit' do
+  @session = Session.find_by_id(params['id'].to_i)
+  erb ("sessions/edit".to_sym)
+end
+
+# SHOW route
+get '/sessions/:id' do
+  @session = Session.find_by_id(params[:id])
+  erb('sessions/show'.to_sym)
+end
+
+
+# UPDATE
+post '/sessions/:id' do
+  @session = Session.new(params)
+  @session.update()
+  redirect ('/sessions')
+end
+
+# CREATE
+post '/sessions' do
+  @session = Session.new(params)
+  @session.save()
+  redirect ('/sessions')
+end
